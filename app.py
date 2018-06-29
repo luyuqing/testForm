67bdca80fe8211e31ba388db6f685476be4b8486
Yuqing Lu <luyuqing0708@gmail.com>
2018-06-29 22:52:14 +0200

    made it

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request, jsonify
-from werkzeug.datastructures import MultiDict
+from flask import Flask, render_template, request, jsonify, redirect
 
-from model import UserForm
+from model import UserForm, ImportForm, AnswerForm
 
 
 app = Flask(__name__)
@@ -9,32 +8,69 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = UserForm()
+    import_form = ImportForm()
+    user_form = UserForm()
+    answer_form = AnswerForm()
     if request.method == 'POST':
-        m = MultiDict([('name', 'lala')])
-        form = UserForm(m)
+        if 'submit1' in request.form:
+            file_name = request.form['file_name']
+            if file_name == 'f1':
+                fill_data = {'name': 'lalala', 'hobby': 'karate'}
+                user_form = UserForm(data=fill_data)
+            elif file_name == 'f2':
+                fill_data = {'name': 'keke', 'hobby': 'swim'}
+                user_form = UserForm(data=fill_data)
+            else:
+                pass
+        else:
+            name = request.form['name']
+            hobby = request.form['hobby']
+            user_form = UserForm(data={'name': name, 'hobby': hobby})
+            new = 'hello ' + name + ' likes ' + hobby
+            answer_form = AnswerForm(data={'answer': new})
+    return render_template("view.html", import_form=import_form, user_form=user_form, answer_form=answer_form)
 
-    return render_template("view.html", form=form)
+
+@app.route('/save', methods=['POST'])
+def save_file():
+    name = request.form['name']
+    hobby = request.form['hobby']
+    print("#####################")
+    print(name)
+    final = name + hobby
+    with open("file.text", "w") as f:
+        f.write(final)
+    return jsonify({"name": name, "hobby": hobby})
+
+# not necessary anymore
+@app.route('/import/<filename>', methods=['GET', 'POST'])
+def import_file(filename):
+    import_form = ImportForm()
+
+    if filename == 'f1':
+        fill_data = {'name': 'lalala', 'hobby': 'karate'}
+        user_form = UserForm(data=fill_data)
+    elif filename == 'f2':
+        fill_data = {'name': 'keke', 'hobby': 'swim'}
+        user_form = UserForm(data=fill_data)
+    else:
+        user_form = UserForm()
+    answer_form = AnswerForm()
+    if request.method == 'POST':
+        name = request.form['name']
+        hobby = request.form['hobby']
+        user_form = UserForm(data={'name': name, 'hobby': hobby})
+        new = 'hello ' + name + 'likes ' + hobby
+        answer_form = AnswerForm(data={'answer': new})
+    return render_template("view.html", import_form=import_form, user_form=user_form, answer_form=answer_form)
 
 
-@app.route('/hello', methods=['POST'])
+# not necessary anymore
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     name = request.form['name']
     new = 'hello ' + name
     return jsonify({"new": new})
 
-# @app.route('/show', methods=['POST'])
-# def show():
-#     form = UserForm(request.form)
-#     name = form.name.data
-#     age = form.age.data
-#     return jsonify({"name": name, "age": age})
-
-
-# if request.method == 'POST':
-#         m = MultiDict([("name", "lala"), ("age", 3)])
-#         form = UserForm(m)
-#         return jsonify(dict(m))
-#     return render_template("view.html", form=form)
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -1,8 +1,20 @@
-from wtforms import Form, IntegerField, StringField, validators, SubmitField
+from wtforms import Form, IntegerField, StringField, \
+    validators, SubmitField, TextAreaField, SelectField
+
+
+class ImportForm(Form):
+    file_name = SelectField('file', choices=[(None, None),
+                                             ('f1', 'f1'),
+                                             ('f2', 'f2')])
+    submit1 = SubmitField('submit1')
 
 
 class UserForm(Form):
     name = StringField('name', validators=[validators.InputRequired()])
-    # age = IntegerField('age', validators=[validators.InputRequired()])
-    submit1 = SubmitField('submit1')
-    # submit2 = SubmitField('submit2')
+    hobby = SelectField('hobby', choices=[('swim', 'swim'),
+                                          ('karate', 'karate')])
+    submit2 = SubmitField('submit2')
+
+
+class AnswerForm(Form):
+    answer = TextAreaField('answer')
