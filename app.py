97f6a116456db77ca406598cd4068409367db15f
Yuqing Lu <luyuqing0708@gmail.com>
2018-06-29 06:43:29 +0200

    some fix

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+from werkzeug.datastructures import MultiDict
 
 from model import UserForm
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    form_fields = UserForm(request.form)
-    return render_template("view.html", form_fields=form_fields)
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = UserForm()
+    if request.method == 'POST':
+        m = MultiDict([('name', 'lala')])
+        form = UserForm(m)
+
+    return render_template("view.html", form=form)
+
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    name = request.form['name']
+    new = 'hello ' + name
+    return jsonify({"new": new})
+
+# @app.route('/show', methods=['POST'])
+# def show():
+#     form = UserForm(request.form)
+#     name = form.name.data
+#     age = form.age.data
+#     return jsonify({"name": name, "age": age})
+
+
+# if request.method == 'POST':
+#         m = MultiDict([("name", "lala"), ("age", 3)])
+#         form = UserForm(m)
+#         return jsonify(dict(m))
+#     return render_template("view.html", form=form)
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -1,5 +1,8 @@
-from wtforms import Form, IntegerField, StringField, validators
+from wtforms import Form, IntegerField, StringField, validators, SubmitField
+
 
 class UserForm(Form):
     name = StringField('name', validators=[validators.InputRequired()])
-    age = IntegerField('age', validators=[validators.InputRequired()])
+    # age = IntegerField('age', validators=[validators.InputRequired()])
+    submit1 = SubmitField('submit1')
+    # submit2 = SubmitField('submit2')
